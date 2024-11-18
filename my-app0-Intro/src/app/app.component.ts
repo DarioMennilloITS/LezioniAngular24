a2cac98bc25ff8f0da8ec52e2b54f98e377dd631
DarioMennilloITS <dario.mennillo@its-ictpiemonte.it>
2024-11-18 12:22:47 +0100

    Add files via upload

---
+++ my-app0-Intro/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PrimoComponentComponent } from './primo-component/primo-component.component';
+import { SecondoComponent } from "./secondo/secondo.component";
+
+//@Component si chiama Decorator ed è una caratteristica di Typescript
+//Il component definisce attrraverso un oggetto JS come è fatta la classe sottostante e di che "tipo" è
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PrimoComponentComponent, SecondoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+  title: string = 'my-app0-Intro';
+
+  nomeSviluppatore: string = "Dario";
+  corso: string = "Web Developer";
+  presenza: boolean = true;
+  sede: string = "Piazza dei Mestieri"
+
+  constructor(){
+    console.log(`Mi chiamo ${this.nomeSviluppatore}, frquento il corso ${this.corso}. Oggi sono presente: ${this.presenza}. Nella sede di ${this.sede}`);
+  }
+}
---
+++ my-app0-Intro/src/app/primo-component/primo-component.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+import { SecondoComponent } from "../secondo/secondo.component";
+
+@Component({
+    selector: "primo-comp",
+    standalone: true,
+    imports: [SecondoComponent],
+    templateUrl: "./primo-component.component.html",
+    styleUrl: './primo-component.component.css'
+})
+export class PrimoComponentComponent{
+    sede: string = "Piazza dei Mestieri";
+    citta: string = "Torino";
+    numPartecipanti: number = 28;
+    presenza:boolean = true;
+    respoCorso!: string;
+
+    constructor(){
+        console.log(`Sono in ${this.sede}`);  
+        this.assegnaRespo("Davide");
+    }
+    
+    assegnaRespo(nomeRespo:string){
+        this.respoCorso = nomeRespo;
+    }
+
+    get nomeRespo(){
+        return "Davide"
+    }
+}
